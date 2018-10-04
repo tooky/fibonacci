7099e21085af40e0b58384313e33a5d83fc500b8
Steve Tooke <tooky@cucumber.io>
2018-10-04 21:01:50 +0100

    Set up the skeleton

---
+++ test/fibonacci_test.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+const fibonacci = require('../lib/fibonacci')
+
+describe("fibonacci", () => {
+  it("START HERE", () => {
+    assert(false, "Write your first test here")
+  })
+})
